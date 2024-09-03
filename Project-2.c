aa4fea131132ea8d9dd109dd5cf7c350f44b292b
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-09-03 19:14:35 +0330

    Project-2

---
+++ Project-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("please enter an integer:\n");
+    scanf("%d",&n);
+    printf("\n");
+    printf("you have entered this number : %d ",n);
+    return 0;
+}
