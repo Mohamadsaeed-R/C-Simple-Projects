9ee8a769ac89cba1821e17b4e5e6a100bb8565ef
Mohamadsaeed <rezaei.mohamadsaeed@gmail.com>
2024-11-06 20:39:01 +0330

    Add files via upload

---
+++ Project -3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a , b , c;
+    printf("enter a number :\n");
+    scanf("%d",&a);
+
+
+    printf("enter an other number :\n");
+    scanf("%d",&b);
+
+    c= a+b;
+
+    printf("%d +%d = %d",a,b,c);
+    return 0;
+
+}
